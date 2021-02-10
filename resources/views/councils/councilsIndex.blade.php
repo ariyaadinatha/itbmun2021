84fc6c9ecb67efbcb9238346afcc12a3740c464a
ariyaadinatha <ariyaadinatha21@gmail.com>
2021-02-10 19:14:29 +0700

    logo fix

--- resources/views/councils/councilsIndex.blade.php
+++ resources/views/councils/councilsIndex.blade.php
@@ -29,29 +29,29 @@
         <div class="col-md-8 offset-md-2 col-12">
             <h2>Select one of the councils below</h2>
             <div class="councils-wrapper">
-                <a class="council p-4" href='/councils/UNSC/'>
-                    <img src="{{URL::asset('assets/councils/councilLogos/Edited/White/new/unsc.png')}}" height=200px>
-                    <p class="pt-3">UNSC</p>
+                <a class="council" href='/councils/UNSC/'>
+                    <img src="{{URL::asset('assets/councils/councilLogos/Edited/White/new/unsc.png')}}" >
+                    <p>UNSC</p>
                 </a>
-                <a class="council p-4" href='/councils/CRISIS/'>
-                    <img src="{{URL::asset('assets/councils/councilLogos/Edited/White/new/crisis.png')}}" height=200px>
-                    <p class="pt-3">CRISIS</p>
+                <a class="council" href='/councils/CRISIS/'>
+                    <img src="{{URL::asset('assets/councils/councilLogos/Edited/White/new/crisis.png')}}" >
+                    <p>CRISIS</p>
                 </a>
-                <a class="council p-4" href='/councils/UNCA/'>
-                    <img src="{{URL::asset('assets/councils/councilLogos/Edited/White/new/unca.png')}}" height=200px>
-                    <p class="pt-3">UNCA</p>
+                <a class="council" href='/councils/UNCA/'>
+                    <img src="{{URL::asset('assets/councils/councilLogos/Edited/White/new/unca.png')}}" >
+                    <p>UNCA</p>
                 </a>
-                <a class="council p-4" href='/councils/UNW/'>
-                    <img src="{{URL::asset('assets/councils/councilLogos/Edited/White/new/unwomen.png')}}" height=200px>
-                    <p class="pt-3">UN Women</p>
+                <a class="council" href='/councils/UNW/'>
+                    <img src="{{URL::asset('assets/councils/councilLogos/Edited/White/new/unwomen.png')}}" >
+                    <p>UN Women</p>
                 </a>
-                <a class="council p-4" href='/councils/UNODC/'>
-                    <img src="{{URL::asset('assets/councils/councilLogos/Edited/White/new/unodc.png')}}" height=200px>
-                    <p class="pt-3">UNODC</p>
+                <a class="council" href='/councils/UNODC/'>
+                    <img src="{{URL::asset('assets/councils/councilLogos/Edited/White/new/unodc.png')}}" >
+                    <p>UNODC</p>
                 </a>
-                <a class="council p-4" href='/councils/WorldBank/'>
-                    <img src="{{URL::asset('assets/councils/councilLogos/Edited/White/new/worldbank.png')}}" height=200px>
-                    <p class="pt-3">World Bank</p>
+                <a class="council" href='/councils/WorldBank/'>
+                    <img src="{{URL::asset('assets/councils/councilLogos/Edited/White/new/worldbank.png')}}" >
+                    <p>World Bank</p>
                 </a>
             </div>
         </div>
